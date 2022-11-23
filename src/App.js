25e018edc20e02d00c28d611f59a4ab34abc7e64
amandaremessa <amanda.antunes@remessaonline.com.br>
2022-11-23 09:59:15 -0300

    fix(frontend): removed logo

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React, { useState } from 'react';
 import api from './services/api';
 import './App.css';
 
-import logo from './assets/logo.png';
-
 function App() {
   const [email, setEmail] = useState('');
 
